e4914a0c02d6e464b43d8d9b9db3e6eadee3ea9b
Althamash <althamash.b@gmail.com>
2013-07-18 18:51:04 +0530

    Updated package.json, updated the app

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.get('*', routes.index);
  * Start Server
  */
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
